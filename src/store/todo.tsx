da1f88ad5bac133716c35c99711e62d1b100e451
casetal <ax.bratsun@gmail.com>
2024-10-09 19:12:52 +0500

    Реализован store для todo

--- src/store/todo.tsx
+++ src/store/todo.tsx
@@ -1,5 +1,40 @@
 import { makeAutoObservable } from "mobx"
+import internal from "stream";
+
+interface Item {
+    id: number;
+    name: string;
+    text: string;
+    parentId: number;
+}
 
 class Todo {
+    items: Array<Item> = [];
 
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    addTodo(id: number, name: string, text: string, parentId: number) {
+        this.items.push({
+            id: id,
+            name: name,
+            text: text,
+            parentId: parentId
+        });
+    }
+
+    removeTodo(id: number) {
+        this.items.filter(i => i.id != id);
+    }
+
+    changeTodo(id: number, name: string, text: string) {
+        this.items.map(i =>
+            (i.id === id) ? {
+                ...i,
+                name: name,
+                text: text
+            } : i
+        )
+    }
 }
